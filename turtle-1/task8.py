a489468285d73847f2136b7c63a8f611629472b5
rudenkoAD <rudenko.ad@phystech.edu>
2021-09-27 16:14:00 +0300

    deleted extra folders

--- turtle-1/task8.py
+++
@@ -1,9 +0,0 @@
-import turtle as T
-import numpy as np
-T.shape('turtle')
-
-step=10
-while True:
-    T.fd(step)
-    T.left(90)
-    step+=2
--- Картинки на выставку (часть 1) /task2.py
+++ Картинки на выставку (часть 1) /task2.py
@@ -1,26 +1,25 @@
 import pygame
+from pygame.draw import *
 
 pygame.init()
-from pygame.draw import *
-screen = pygame.display.set_mode((300, 200))
 
 FPS = 30
 screen = pygame.display.set_mode((400, 400))
 
-rect(screen, (255, 0, 255), (100, 100, 200, 200))
-rect(screen, (0, 0, 255), (100, 100, 200, 200), 5)
-polygon(screen, (255, 255, 0), [(100,100), (200,50),
-                               (300,100), (100,100)])
-polygon(screen, (0, 0, 255), [(100,100), (200,50),
-                               (300,100), (100,100)], 5)
-circle(screen, (0, 255, 0), (200, 175), 50)
-circle(screen, (255, 255, 255), (200, 175), 50, 5)
-
+x1 = 100; y1 = 100
+x2 = 300; y2 = 200
+N = 10
+color = (255, 255, 255)
+rect(screen, color, (x1, y1, x2 - x1, y2 - y1), 2)
+h = (x2 - x1) // (N + 1)
+x = x1 + h
+for i in range(N):
+    line(screen, color, (x, y1), (x, y2))
+    x += h
 
 pygame.display.update()
 clock = pygame.time.Clock()
 finished = False
-
 
 while not finished:
     clock.tick(FPS)
